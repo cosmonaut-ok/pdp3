e0bb8ed956b5a36dd298564d7b0b0fb09d472d8c
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2017-10-12 23:42:19 +0300

    refactoring continue

--- Boundary_Maxwell_conditions.h
+++ Boundary_Maxwell_conditions.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "E_field.h"
-#include"current.h"
+#include "current.h"
+
 class Boundary_Maxwell_conditions
 {
 public:
-E_field* e_fld;
+	E_field* e_fld;
 
 public:
-	Boundary_Maxwell_conditions(void );
-	Boundary_Maxwell_conditions(E_field* e_fld );
+	Boundary_Maxwell_conditions(void);
+	Boundary_Maxwell_conditions(E_field* e_fld);
 	~Boundary_Maxwell_conditions(void);
 	void specify_initial_field(Geometry* cyl_geom,flcuda E_fi_upper, flcuda E_fi_left, flcuda E_fi_right);
 	void radiation_source(Geometry* cyl_geom, flcuda region_size, flcuda frequency, int type, flcuda time);
