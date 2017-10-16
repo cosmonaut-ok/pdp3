81fc0de0bc7c877663da120a9d7ece7075b120ec
Alexander Vynnyk <cosmonaut-ok@users.noreply.github.com>
2017-10-17 02:44:22 +0300

    Refactoring 4 (#10)
    * dev
    
    * Remove not used Beam inclusions
    
    * dev
    
    * dev
    
    * rename Time.cpp to pdp3_time.cpp
    
    * dev

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,3 @@
-#include<iostream>
-#include "Constant.h"
-#include "field.h"
-#include "E_field.h"
-#include "H_field.h"
-#include "pdp3_time.h"
-#include "Particles.h"
-#include "Fourier.h"
-#include "Poisson.h"
-#include "poisson_neumann.h"
-#include "poisson_dirichlet.h"
-#include "particles_list.h"
-#include <fstream>
-#include <math.h>
-
-#include "Boundary_Maxwell_conditions.h"
-#include "input_output_class.h"
-#include "Beam.h"
-#include "Bunch.h"
-#include "time.h"
-#include "particles_struct.h"
 #include "Load_init_param.h"
 
 using namespace std;
--- boundary_dirichlet.cpp
+++ boundary_dirichlet.cpp
@@ -9,6 +9,7 @@ boundary_dirichlet::boundary_dirichlet(E_field* ef_t,
                                                           cyl_geom(geom_t)
 {
 }
+
 boundary_dirichlet::boundary_dirichlet(void)
 {
 }
@@ -16,38 +17,34 @@ boundary_dirichlet::boundary_dirichlet(void)
 boundary_dirichlet::~boundary_dirichlet(void)
 {
 }
-void boundary_dirichlet:: specify_boundary_conditions(double E_fi_upper,
-                                                      double E_fi_left,
-                                                      double E_fi_right,
-                                                      double fi_upper_wall)
+
+void boundary_dirichlet::specify_boundary_conditions(double e_fi_upper,
+                                                     double e_fi_left,
+                                                     double e_fi_right,
+                                                     double fi_upper_wall)
 {
-	int i=0, k=0;
-	int n_grid1=cyl_geom->n_grid_1;
+	int n_grid1 = cyl_geom->n_grid_1;
 	int n_grid2 = cyl_geom->n_grid_2;
-// setazimuthal component electric field initial value
-/////////////////////////////////////////////
-	for (i=0;i<(n_grid1);i++)
-	{
-		e_f->e2[i][0]=E_fi_left;
-		e_f->e2[i][n_grid2-1]=E_fi_right;
-	}
-	for(k=0;k<n_grid2;k++)
-	{
-		e_f->e2[n_grid1-1][k]=E_fi_upper;
-	}
-/////////////////////////////////////////////
 
-////////////////////////////////////////////
+	// set azimuthal component electric field initial value
+	for (int i=0; i<(n_grid1); i++)
+	{
+		e_f->e2[i][0] = e_fi_left;
+		e_f->e2[i][n_grid2-1] = e_fi_right;
+	}
+	for(int k=0; k<n_grid2; k++)
+	{
+		e_f->e2[n_grid1-1][k] = e_fi_upper;
+	}
+
 	//set potential initial value in z wall
-	for(k=0;k<n_grid2;k++)
+	for(int k=0; k<n_grid2; k++)
 	{
-		e_f->fi[n_grid1-1][k]=fi_upper_wall;
+		e_f->fi[n_grid1-1][k] = fi_upper_wall;
 	}
-///////////////////////////////////////////
-	// calculate poisson equation with Neumann boundary conditions
 
+	// calculate poisson equation with Neumann boundary conditions
 	Poisson* solve = new Poisson_dirichlet(cyl_geom);
-	solve->poisson_solve(e_f,rho);
-	solve->test_poisson_equation(e_f,rho);
-/////////////////////////////////////////
+	solve->poisson_solve(e_f, rho);
+	solve->test_poisson_equation(e_f, rho);
 }
