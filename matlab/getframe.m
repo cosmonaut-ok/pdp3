4c42570be6e728b3e40171015fbd91549d581e08
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2017-10-31 18:30:23 +0200

    dev

---
+++ matlab/getframe.m
@@ -0,0 +1,31 @@
+## Copyright (C) 2011 Philip
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## getframe
+
+## Author: Philip <address@hidden>
+## Created: 2011-12-14
+
+function [ ret ] = getframe (h)
+
+  print (h, "tmp.fig", "-dppm");
+  ret = im2double (imread ("tmp.fig"));
+  ## Truncate to even size to accomodate addframe()
+  if (mod (size (ret, 1), 2) > 0); ret = ret(2:end, :, :); endif
+  if (mod (size (ret, 2), 2) > 0); ret = ret(:, 2:end, :); endif
+
+endfunction
+
--- octave/rho_movie_create_light3.m
+++ octave/rho_movie_create_light3.m
@@ -25,21 +25,13 @@ function var = rho_movie_create_light3(data_path, video_path, file_delta, size_1
   end
 
   pkg load 'image';
-
-####################################################################################################
+  pkg load 'video';
 
   data_file_e1 = strcat(data_path, '/', 'e1');
   data_file_e3 = strcat(data_path, '/', 'e3');
   data_file_rho_beam = strcat(data_path, '/', 'rho_beam');
 
   filter = ones(3,12)/3/12;
-
-  path4wr = video_path;
-  name4wr = 'h2';
-
-  t_begin = 1.6e-6;
-  dt = 1e-9;
-  var = 'rho_sp';
 
   f = figure;
   set(f, 'Position', [50 60 1050 700], 'Color', 'white');
@@ -63,14 +55,15 @@ function var = rho_movie_create_light3(data_path, video_path, file_delta, size_1
   im3 = image(z,'Parent',a3, 'CDataMapping', 'scaled');
   set(a3, 'Clim', clim3);
   %%---------------
-  ## colormap('gray');
+  %% colormap('gray');
+
+  movie_filename = strcat(video_path,'/field_movie.avi');
+  movie_file = avifile(movie_filename, 'codec', 'msmpeg4v2');
 
   N = file_delta;
 
-####################################################################################################
-
   for k = 0:100
-    disp(['Processing frame ', num2str(k)]);
+    disp(['Loading files set ', num2str(k)]);
 
     tstart = (k)*N;
     tend = ((k+1)*N-1);
@@ -79,47 +72,34 @@ function var = rho_movie_create_light3(data_path, video_path, file_delta, size_1
     fidh_e1 = fopen([data_file_e1 num2str(k)], 'r');
     fidh_e3 = fopen([data_file_e3 num2str(k)], 'r');
     fidh_rho_beam = fopen([data_file_rho_beam num2str(k)], 'r');
-    ##
-    ## %e is an exponential notation
-    try
-      h_field_e1 = fscanf(fidh_e1, '%e', size_1*size_3*file_delta); % TODO: why do we times all values?
-    catch
-      continue;
-    end_try_catch
+    %%
+    %% %e is an exponential notation
+    h_field_e1 = fscanf(fidh_e1, '%e', size_1*size_3*file_delta); % TODO: why do we times all values?
+    h_field_e3 = fscanf(fidh_e3, '%e', size_1*size_3*file_delta);
+    h_field_rho_beam = fscanf(fidh_rho_beam, '%e', size_1*size_3*file_delta);
 
-    try
-      h_field_e3 = fscanf(fidh_e3, '%e', size_1*size_3*file_delta);
-    catch
-      continue;
-    end_try_catch
-
-    try
-      h_field_rho_beam = fscanf(fidh_rho_beam, '%e', size_1*size_3*file_delta);
-    catch
-      return;
-    end_try_catch
-
-    ##
+    %%
     fclose(fidh_e1);
     fclose(fidh_e3);
     fclose(fidh_rho_beam);
     length(h_field_e1);
 
+    disp(['Loaded files set ', num2str(k)]);
+
+    disp(['Processng files set ', num2str(k)]);
     for t = tstart:tend
       local_step = mod(t, file_delta);
 
-      try
-        h_field_shot1 = h_field_e1(size_1*size_3*local_step+1:size_1*size_3*(local_step+1));
-        h_field_matrix1 = fliplr(reshape(h_field_shot1,size_3,size_1))';
+      disp(['Processing ', num2str(t)]);
 
-        h_field_shot2 = h_field_e3(size_1*size_3*flocal_step+1:size_1*size_3*(local_step+1));
-        h_field_matrix2 = fliplr(reshape(h_field_shot2,size_3,size_1))';
+      h_field_shot1 = h_field_e1(size_1*size_3*local_step+1:size_1*size_3*(local_step+1));
+      h_field_matrix1 = fliplr(reshape(h_field_shot1,size_3,size_1))';
 
-        h_field_shot3 = h_field_rho_beam(size_1*size_3*local_step+1:size_1*size_3*(local_step+1));
-        h_field_matrix3 = fliplr(reshape(h_field_shot3,size_3,size_1))';
-      catch
-        continue;
-      end_try_catch
+      h_field_shot2 = h_field_e3(size_1*size_3*local_step+1:size_1*size_3*(local_step+1));
+      h_field_matrix2 = fliplr(reshape(h_field_shot2,size_3,size_1))';
+
+      h_field_shot3 = h_field_rho_beam(size_1*size_3*local_step+1:size_1*size_3*(local_step+1));
+      h_field_matrix3 = fliplr(reshape(h_field_shot3,size_3,size_1))';
 
       set(im1, 'CData', imfilter(h_field_matrix1,filter));
       set(im2, 'CData', imfilter(h_field_matrix2,filter));
@@ -129,109 +109,33 @@ function var = rho_movie_create_light3(data_path, video_path, file_delta, size_1
 
       figHandle = gcf;
       ## frame = getframe(figHandle);
-      ## D(:,:,:,i)   = frame.cdata;
-      ## D(:,:,:,i+1) = frame.cdata;
-      ## D(:,:,:,i+2) = frame.cdata;
-      ## i = i + 3;
+
+      ## D(:,:,:,i)   = figHandle; % frame.cdata;
+      ## D(:,:,:,i+1) = figHandle; % frame.cdata;
+      ## D(:,:,:,i+2) = figHandle; % frame.cdata;
+      %% i = i + 3;
+      addframe(movie_file, figHandle);
 
       drawnow;
 
     end
 
-    ## f2 = figure('Position', [20 20 100 100]);
-    ## at = axes('Parent', f2);
-    ## mov = immovie(D);
-    ## movie_filename = strcat(video_path,'field_movie_',num2str(t/1e-7,'%3.2f'),'.avi');
-    ## movie_object = VideoWriter(movie_filename);
-    ## open(movie_object);
-    ## writeVideo(movie_object, mov);
-    ## clear mov;
-    ## close(movie_object)
-    ## close(f2);
-    ## pack
+    %% f2 = figure('Position', [20 20 100 100]);
+    %% at = axes('Parent', f2);
+    %% mov = immovie(D);
+    %% movie_filename = strcat(video_path,'field_movie_',num2str(t/1e-7,'%3.2f'),'.avi');
+    %% movie_object = VideoWriter(movie_filename);
+    %% open(movie_object);
+    %% writeVideo(movie_object, mov);
+
+    addframe(movie_file, mov);
+
+    %% clear mov;
+    %% close(movie_object)
+    %% close(f2);
+    %% pack
   end
+
+  clear movie_file
+
 endfunction
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  ## y = y .^2;
-  ## plot(y);
-  ## x = 0:20;
-  ## t = t .^2;
-  ## scatter(x, t, ".");
-
-  ## x = 0:0.01:3;
-  ## hf = figure ();
-  ## plot (x, erf (x));
-  ## hold on;
-  ## plot (x, x, "r");
-  ## axis ([0, 3, 0, 1]);
-  ## text (0.65, 0.6175, ['$\displaystyle\leftarrow x = {2\over\sqrt{\pi}}'...
-  ##                        '\int_{0}^{x}e^{-t^2} dt = 0.6175$']);
-  ## xlabel ("x");
-  ## ylabel ("erf (x)");
-  ## title ("erf (x) with text annotation");
-  ## set (hf, "visible", "off");
-  ## print (hf, "plot15_7.pdf", "-dpdflatexstandalone");
-  ## set (hf, "visible", "on");
-  ## system ("pdflatex plot15_7");
-  ## open ("plot15_7.pdf");
-
-## x = lsode("lorenz", [3;15;1], (0:0.01:25)');
-## gset parametric
-## gsplot x
