76776ba4c4a9b6cbdd8c9cec84200f75b2ce9811
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2018-01-17 23:55:34 +0200

    Fix cell calculation, Makefile

--- E_field.cpp
+++ E_field.cpp
@@ -399,6 +399,9 @@ Triple E_field::get_field(double x1, double x3)
 ///////////////////////////////////////////////////
   //finding number of cell. example dr=0.5,    x1 = 0.7, i_r =0;!!
   i_r = (int)ceil((x1-0.5*dr)/geom1->dr)-1;
+  // TODO: workaround: sometimes it gives -1.
+  // Just get 0 cell if it happence
+  if (i_r < 0) { i_r = 0; }
   k_z = (int)ceil((x3)/geom1->dz)-1;
 
   vol_1 = PI*dz*dr*dr*(2*i_r+1);
@@ -426,6 +429,9 @@ Triple E_field::get_field(double x1, double x3)
 ///////////////////////////////////////////////////////
 //finding number of cell. example dz=0.5,    x3 = 0.7, z_k =0;!!
   i_r = (int)ceil((x1)/geom1->dr)-1;
+  // TODO: workaround: sometimes it gives -1.
+  // Just get 0 cell if it happence
+  if (i_r < 0) { i_r = 0; }
   k_z = (int)ceil((x3-0.5*dz)/geom1->dz)-1;
 
 ///////////////////////////////////
@@ -462,6 +468,9 @@ Triple E_field::get_field(double x1, double x3)
 ///////////////////////////////////////////////////////
   //finding number of cell. example dz=0.5,    x3 = 0.7, z_k =1;
   i_r = (int)ceil((x1)/geom1->dr)-1;
+  // TODO: workaround: sometimes it gives -1.
+  // Just get 0 cell if it happence
+  if (i_r < 0) { i_r = 0; }
   k_z = (int)ceil((x3)/geom1->dz)-1;
 
   if(x1>dr)
