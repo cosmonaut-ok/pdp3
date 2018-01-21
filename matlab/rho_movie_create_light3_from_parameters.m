e1ea7a04160787347aa615e9af2b671010c76ea3
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2018-01-21 02:53:33 +0200

    Add params to save data, update notifications

--- matlab/rho_movie_create_light3_from_parameters.m
+++ matlab/rho_movie_create_light3_from_parameters.m
@@ -7,14 +7,14 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   if ~exist('clim_e3', 'var')
     clim_e3 = [-2e3 2e3];
   end
-  
+
   config_path = fileparts(xml_config_file);
-  
+
   video_path = config_path;
   video_file_name = 'field_movie.avi';
-  
-  file_delta = 100;
-  
+
+  %% file_delta = 100;
+
   dom_node = xmlread(xml_config_file);
   dom_root = dom_node.getDocumentElement;
   %% get geometry
@@ -34,10 +34,11 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   bunch_density = str2double(particles.item(0).getElementsByTagName('density').item(0).getFirstChild.getData);
 
   clim_rho_beam = [-(bunch_density*1.6e-19) 0];
-  
+
   %% get file to save parameters
   file_save_parameters = dom_root.getElementsByTagName('file_save_parameters');
   local_data_path = file_save_parameters.item(0).getElementsByTagName('path_to_result').item(0).getFirstChild.getData;
+  frames_per_file = str2num(file_save_parameters.item(0).getElementsByTagName('frames_per_file').item(0).getFirstChild.getData);
 
   %% disp(class(char(local_data_path)));
   if startsWith(local_data_path, '/')
@@ -49,7 +50,7 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   data_file_e1 = strcat(data_path, '/', 'e1');
   data_file_e3 = strcat(data_path, '/', 'e3');
   data_file_rho_beam = strcat(data_path, '/', 'rho_beam');
-  
+
   %% filter = ones(3,12)/3/12; % matrix 12x3, all elements equal to 0.027778
 
   % create figure window (and place it as current figure)
@@ -62,7 +63,7 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   a1 = axes('Parent', f);
   a2 = axes('Parent', f);
   a3 = axes('Parent', f);
-  
+
   %% place axes on figure (normalized to whole figure window)
   %% set(a1, 'Unit', 'normalized', 'Position', [0.1 0.74 0.8 0.2]);
   %% set(a2, 'Unit', 'normalized', 'Position', [0.1 0.41 0.8 0.2]);
@@ -74,10 +75,10 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   %%
   %% initial---
   %%
-  
+
   %%%% initializing images and axes
   z = zeros(size_1, size_3);
-  
+
   %% set number of marks with names in X and Y axes
   x_ticks_number = 10;
   y_ticks_number = 4;
@@ -86,10 +87,10 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   y_axe_title = 'R(m)';
   colorbar_title = 'V/m';
   x_tick_range = 0:x_tick_max/x_ticks_number:x_tick_max; % we need 10 (or x_ticks_number) ticks
-  x_tick_gird_size = 0:size_3/x_ticks_number:size_3;     % from 0 to x_tick_max. it's required 
+  x_tick_gird_size = 0:size_3/x_ticks_number:size_3;     % from 0 to x_tick_max. it's required
   y_tick_range = 0:y_tick_max/y_ticks_number:y_tick_max; % to convert gird to real size (meters)
   y_tick_gird_size = 0:size_1/y_ticks_number:size_1;     % Same for X and Y axes
-  
+
   im1 = image(z,'Parent', a1, 'CDataMapping', 'scaled');
   %% set color limits
   a1.CLim = clim_e1;
@@ -109,7 +110,7 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   a1.XTickLabel = x_tick_range;
   a1.YTick = y_tick_gird_size;
   a1.YTickLabel = y_tick_range;
-  
+
   im2 = image(z,'Parent', a2, 'CDataMapping', 'scaled');
   %% set color limits
   a2.CLim = clim_e3;
@@ -129,7 +130,7 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   a2.XTickLabel = x_tick_range;
   a2.YTick = y_tick_gird_size;
   a2.YTickLabel = y_tick_range;
-  
+
   im3 = image(z,'Parent', a3, 'CDataMapping', 'scaled');
   %% set color limits
   a3.CLim = clim_rho_beam;
@@ -154,7 +155,7 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   a3.XTickLabel = x_tick_range;
   a3.YTick = y_tick_gird_size;
   a3.YTickLabel = y_tick_range;
-  
+
   %%---------------
   colormap('gray');
 
@@ -162,13 +163,13 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
   movie_object = VideoWriter(movie_filename);
   open(movie_object);
 
-  N = file_delta; % 100 by default
+  N = frames_per_file; % 100 by default
 
   for k = 0:100 % TODO: why to 100?
-    
+
     tstart = k*N; % k=2 -> 200
     tend = ((k+1)*N-1); % k=2 -> 3*100-1 -> 299
-    
+
     i = 1;
 
     if ~(exist([data_file_e1 num2str(k)], 'file') == 2) || ~(exist([data_file_e3 num2str(k)], 'file') == 2) || ~(exist([data_file_rho_beam num2str(k)], 'file') == 2)
@@ -177,16 +178,16 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
     end
 
     disp(['Loading files set ', num2str(k)]);
-    
+
     %% Open data files
     fidh_e1 = fopen([data_file_e1 num2str(k)], 'r');
     fidh_e3 = fopen([data_file_e3 num2str(k)], 'r');
     fidh_rho_beam = fopen([data_file_rho_beam num2str(k)], 'r');
 
     % %e is an exponential notation: ex. 5e2
-    h_field_e1 = fscanf(fidh_e1, '%e', size_1*size_3*file_delta); % TODO: why do we times all values?
-    h_field_e3 = fscanf(fidh_e3, '%e', size_1*size_3*file_delta);
-    h_field_rho_beam = fscanf(fidh_rho_beam, '%e', size_1*size_3*file_delta);
+    h_field_e1 = fscanf(fidh_e1, '%e', size_1*size_3*frames_per_file); % TODO: why do we times all values?
+    h_field_e3 = fscanf(fidh_e3, '%e', size_1*size_3*frames_per_file);
+    h_field_rho_beam = fscanf(fidh_rho_beam, '%e', size_1*size_3*frames_per_file);
 
     %% Close data files
     fclose(fidh_e1);
@@ -195,7 +196,7 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
     % length(h_field_e1);
 
     for t = tstart:tend
-      local_step = mod(t, file_delta);
+      local_step = mod(t, frames_per_file);
 
       disp(['Processing frame ', num2str(local_step)]);
 
@@ -211,7 +212,7 @@ function var = rho_movie_create_light3_from_parameters(xml_config_file, clim_e1,
       %% set(im1, 'CData', imfilter(h_field_matrix1,filter));
       %% set(im2, 'CData', imfilter(h_field_matrix2,filter));
       %% set(im3, 'CData', imfilter(h_field_matrix3,filter));
-      
+
       set(im1, 'CData', h_field_matrix1);
       set(im2, 'CData', h_field_matrix2);
       set(im3, 'CData', h_field_matrix3);
