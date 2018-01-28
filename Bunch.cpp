e073091bfbfee56d8c9c55840f06d1be27927fc3
Alexander Vynnyk <cosmonaut-ok@users.noreply.github.com>
2018-01-28 16:11:26 +0200

    Refactoring 7 (#17)
    * remove Bunch::bunch_inject_calc_E as unused
    
    * remove H_field::magnetostatic_equation as unused
    
    * remove wrapper.cpp and wrapper.h as unused
    
    * remove some opencl bindings as unused
    
    * remove some comments as unused or not informative
    
    * remove Fourier::fastcosinetransform_old as unused
    
    * add more parallelization
    
    * rename Particles::velocity_distribution_v2 to Particles::velocity_distribution and remove old version Particles::velocity_distribution as unused
    
    *  remove commendted out Particles::get_cell_numbers_jr

--- Bunch.cpp
+++ Bunch.cpp
@@ -28,7 +28,7 @@ Bunch::Bunch(char* p_name,
   charge *= n_in_big;
   mass *= n_in_big;
 
-#pragma omp parallel
+#pragma omp parallel for
   for(int i=0; i<number; i++)
   {
     is_alive[i] = 0;
@@ -83,59 +83,6 @@ void Bunch::bunch_inject(Time* time)
   }
 }
 
-// TODO: seems, not used
-void Bunch::bunch_inject_calc_E(Geometry* geom,
-                                E_field* E_beam,
-                                E_field* E,
-                                Time* time)
-{
-  double dl = vel_bunch*time->delta_t;
-  int step_num =  duration/time->delta_t;
-  int particles_in_step = number/step_num;
-  int start_number = time->current_time/time->delta_t*particles_in_step;
-  double dr = geom1->dr*1.00000001; // TODO: WTF?
-  double dz = geom1->dz*1.00000001;
-  if (time->current_time<duration)
-    for(int i = 0; i < particles_in_step; i++)
-    {
-      double  rand_r = random_reverse(i, 3); // TODO: why 3 and 5?
-      double  rand_z = random_reverse(i, 5);
-      x1[i+start_number] = (radius)*sqrt(rand_r) + dr/2.0;
-
-      x3[i+start_number] = dl*(rand_z)+dz/2.0;;
-      v3[i+start_number] =vel_bunch;
-      v1[i+start_number] = 1e5; // TODO: magic number
-      is_alive[i+start_number] = true;
-    }
-  if (time->current_time==0)
-  {
-    // calculational field of elementary beam portion
-    charge_density rho_beam(geom);
-    charge_weighting(&rho_beam);
-    Poisson_dirichlet dirih(geom);
-    dirih.poisson_solve(E_beam, &rho_beam);
-  }
-  ////Er////
-  for(int i=0;i<(geom1->n_grid_1-1);i++)
-    for(int k=0;k<(geom1->n_grid_2-1);k++)
-    {
-      E->e1[i][k]= E->e1[i][k]+E_beam->e1[i][k];
-    }
-  ///Ef////
-  for(int i=0;i<(geom1->n_grid_1-1);i++)
-    for(int k=0;k<(geom1->n_grid_2-1);k++)
-    {
-      E->e2[i][k]= E->e2[i][k]+E_beam->e2[i][k];
-    }
-  ///Ez////
-  for(int i=0;i<(geom1->n_grid_1-1);i++)
-    for(int k=0;k<(geom1->n_grid_2-1);k++)
-    {
-      E->e3[i][k]= E->e3[i][k]+E_beam->e3[i][k];
-    }
-
-}
-
 void Bunch::half_step_coord(Time* t)
 {
   double dr = geom1->dr;
@@ -147,6 +94,7 @@ void Bunch::half_step_coord(Time* t)
   double x1_wallX2 = x1_wall*2.0;
   // double x3_wallX2 = x3_wall*2.0;
   double half_dt = t->delta_t/2.0;
+
 #pragma omp parallel for shared(dr, dz, x1_wall, x3_wall, half_dr, half_dz, half_dt, x1_wallX2)
   for(int i=0;i<number;i++)
     if (is_alive[i])
---
+++ H_field.cpp
@@ -0,0 +1,338 @@
+#include "H_field.h"
+#include "E_field.h"
+#include <cstdlib>
+#include <math.h>
+#include "Constant.h"
+
+using namespace constant;
+
+// Constructor
+H_field::H_field(Geometry* geom1_l):geom1(geom1_l)
+{
+  // Hr
+  h1 = new double* [geom1->n_grid_1];
+#pragma omp parallel for shared (h1)
+  for (int i=0;i<(geom1->n_grid_1);i++)
+    h1[i] = new double[geom1->n_grid_2-1];
+  h1_1d = new double[geom1->n_grid_1 * (geom1->n_grid_2 - 1)];
+
+  // Hf
+  h2 = new double* [geom1->n_grid_1-1];
+#pragma omp parallel for shared (h2)
+  for (int i=0;i<(geom1->n_grid_1-1);i++)
+    h2[i] = new double[geom1->n_grid_2-1];
+  h2_1d = new double[(geom1->n_grid_1 - 1) * (geom1->n_grid_2 - 1)];
+
+  // Hz
+  h3 = new double* [geom1->n_grid_1-1];
+#pragma omp parallel for shared (h3)
+  for (int i=0;i<(geom1->n_grid_1-1);i++)
+    h3[i] = new double[geom1->n_grid_2];
+  h3_1d = new double[(geom1->n_grid_1 - 1) * geom1->n_grid_2];
+
+  // Hr_half_time
+  h1_half_time = new double* [geom1->n_grid_1];
+#pragma omp parallel for shared (h1_half_time)
+  for (int i=0;i<(geom1->n_grid_1);i++)
+    h1_half_time[i] = new double[geom1->n_grid_2-1];
+
+  // Hf half time
+  h2_half_time = new double* [geom1->n_grid_1-1];
+#pragma omp parallel for shared (h2_half_time)
+  for (int i=0;i<(geom1->n_grid_1-1);i++)
+    h2_half_time[i] = new double[geom1->n_grid_2-1];
+
+  // Hz half time
+  h3_half_time = new double* [geom1->n_grid_1-1];
+#pragma omp parallel for shared (h3_half_time)
+  for (int i=0;i<(geom1->n_grid_1-1);i++)
+    h3_half_time[i] = new double[geom1->n_grid_2];
+
+  // Ar
+  Ar = new double* [geom1->n_grid_1];
+#pragma omp parallel for shared (Ar)
+  for (int i=0;i<(geom1->n_grid_1);i++)
+    Ar[i] = new double[geom1->n_grid_2];
+
+  // Afi
+  Afi = new double* [geom1->n_grid_1];
+#pragma omp parallel for shared (Afi)
+  for (int i=0;i<(geom1->n_grid_1);i++)
+    Afi[i] = new double[geom1->n_grid_2];
+
+  // Az
+  Az = new double* [geom1->n_grid_1];
+#pragma omp parallel for shared (Az)
+  for (int i=0;i<(geom1->n_grid_1);i++)
+  {
+    Az[i] = new double[geom1->n_grid_2];
+  }
+
+  // initialisation of magnetic potential
+#pragma omp parallel shared (h1, h2, h3, h1_half_time, h2_half_time, h3_half_time)
+  {
+#pragma omp for
+    for(int i=0;i<geom1->n_grid_1-1;i++)
+      for(int k=0;k<geom1->n_grid_2-1;k++)
+      {
+        h1[i][k]=0;
+        h2[i][k]=0;
+        h3[i][k]=0;
+        h1_half_time[i][k]=0;
+        h2_half_time[i][k]=0;
+        h3_half_time[i][k]=0;
+      }
+#pragma omp for
+    for(int k=0;k<geom1->n_grid_2-1;k++)
+    {
+      h1[geom1->n_grid_1-1][k]=0;
+      h1_half_time[geom1->n_grid_1-1][k]=0;
+    }
+  }
+}
+// Destructor
+H_field::~H_field(void)
+{
+  for (int i=0; i<(geom1->n_grid_1);i++)
+  {
+    delete[]h1[i];
+    delete[]h1_half_time[i];
+  }
+  delete[]h1;
+  delete[]h1_half_time;
+
+  for (int i=0; i<(geom1->n_grid_1-1);i++)
+  {
+    delete[]h2[i];
+    delete[]h2_half_time[i];
+  }
+  delete[]h2;
+  delete[]h2_half_time;
+
+  for (int i=0; i<(geom1->n_grid_1-1);i++)
+  {
+    delete[]h3[i];
+    delete[]h3_half_time[i];
+  }
+  delete[]h3;
+  delete[]h3_half_time;
+
+  for (int i=0; i<(geom1->n_grid_1-1);i++)
+    delete[]Ar[i];
+  delete[]Ar;
+
+  for (int i=0; i<(geom1->n_grid_1-1);i++)
+    delete[]Afi[i];
+  delete[]Afi;
+
+  for (int i=0; i<(geom1->n_grid_1-1);i++)
+    delete[]Az[i];
+  delete[]Az;
+
+}
+
+void H_field::set_homogeneous_h(double H1, double H2, double H3)
+{
+#pragma omp parallel shared (H1, H2, H3)
+  {
+#pragma omp for
+    for (int i=0;i<geom1->n_grid_1;i++)
+      for (int k=0;(k<geom1->n_grid_2-1);k++)
+      {
+        //if (h1[i][k]!= NULL)
+        h1[i][k]=H1;
+        h1_half_time[i][k]=H1;
+      }
+#pragma omp for
+    for (int i=0;i<(geom1->n_grid_1-1);i++)
+      for (int k=0;(k<geom1->n_grid_2-1);k++)
+      {
+        //if (h1[i][k]!= NULL)
+        h2[i][k]=H2;
+        h2_half_time[i][k]=H2;
+      }
+#pragma omp for
+    for (int i=0;i<(geom1->n_grid_1-1);i++)
+      for (int k=0;(k<geom1->n_grid_2);k++)
+      {
+        //if (h1[i][k]!= NULL)
+        h3[i][k]=H3;
+        h3_half_time[i][k]=H3;
+      }
+  }
+}
+
+// Field calculation
+void H_field::calc_field(E_field* e_field1, Time* time1)
+{
+  double alpha;
+
+  // Hr - last i value
+  for(int k=0; k<(geom1->n_grid_2-1); k++)
+  {
+    int i=geom1->n_grid_1-1;
+    alpha=((e_field1->e2[i][k+1]-e_field1->e2[i][k])/geom1->dz)/MAGN_CONST;
+
+    this->h1_half_time[i][k]=this->h1[i][k]+alpha*time1->delta_t/2;
+    this->h1[i][k] = this->h1[i][k]+alpha*time1->delta_t;
+  }
+
+  for(int i=0;i<(geom1->n_grid_1-1);i++)
+    for(int k=0;k<(geom1->n_grid_2-1);k++)
+    {
+      alpha=((e_field1->e2[i][k+1]-e_field1->e2[i][k])/geom1->dz)/MAGN_CONST;
+
+      this->h1_half_time[i][k]=this->h1[i][k]+alpha*time1->delta_t/2;
+      this->h1[i][k] = this->h1[i][k]+alpha*time1->delta_t;
+
+      alpha=((e_field1->e3[i+1][k]-e_field1->e3[i][k])/geom1->dr -
+             (e_field1->e1[i][k+1]-e_field1->e1[i][k])/geom1->dz)/MAGN_CONST;
+      this->h2_half_time[i][k] = this->h2[i][k]+alpha*time1->delta_t/2;
+      this->h2[i][k] = this->h2[i][k]+alpha*time1->delta_t;
+
+      alpha= ((e_field1->e2[i+1][k]+e_field1->e2[i][k])/(2.0*geom1->dr*(i+0.5)) +
+              (e_field1->e2[i+1][k]-e_field1->e2[i][k])/geom1->dr)/MAGN_CONST;
+      this->h3_half_time[i][k] = this->h3[i][k]-alpha*time1->delta_t/2;
+      this->h3[i][k] = this->h3[i][k]-alpha*time1->delta_t;
+
+    }
+}
+
+// function for magnetic field weighting//
+Triple H_field::get_field(double x1, double x3)
+{
+  int i_r=0; // number of particle i cell
+  int k_z=0; // number of particle k cell
+
+  double dr = geom1->dr;
+  double dz = geom1->dz;
+  double r1, r2, r3; // temp variables for calculation
+  double dz1, dz2; // temp var.: width of k and k+1 cell
+  double hr =0;
+  double hfi =0;
+  double hz =0;
+  double vol_1 =0; // volume of i cell; Q/V, V - volume of elementary cell
+  double vol_2 =0; // volume of i+1 cell;
+
+  r1 = x1-0.5*dr;
+  r3 = x1+0.5*dr;
+
+  //// weighting of H_z
+  //finding number of cell. example dr=0.5, x1 = 0.7, i_r =0;!!
+  i_r = (int)ceil((x1-0.5*dr)/geom1->dr)-1;
+  // TODO: workaround: sometimes it gives -1.
+  // Just get 0 cell if it happence
+  if (i_r < 0) { i_r = 0; }
+  k_z = (int)ceil((x3)/geom1->dz)-1;
+
+  vol_1 = PI*dz*dr*dr*(2*i_r+1);
+  vol_2 = PI*dz*dr*dr*(2*i_r+3);
+  dz1 = (k_z+1)*dz-x3;
+  dz2 = x3 - k_z*dz;
+  r2 = (i_r+1)*dr;
+
+  // weighting Hz[i][k]
+  hz = hz + h3_half_time[i_r][k_z]*(PI*dz1*(r2*r2-r1*r1))/vol_1;
+
+  // weighting Hz[i+1][k]
+  hz = hz + h3_half_time[i_r+1][k_z]*(PI*dz1*(r3*r3-r2*r2))/vol_2;
+
+  // weighting Hz[i][k+1]
+  hz= hz + h3_half_time[i_r][k_z+1]*(PI*dz2*(r3*r3-r2*r2))/vol_1;
+
+  // weighting Hz[i+1][k+1]
+  hz = hz + h3_half_time[i_r+1][k_z+1]*(PI*dz2*(r3*r3-r2*r2))/vol_2;
+
+  //// weighting of Hr
+  // finding number of cell. example dz=0.5, x3 = 0.7, z_k =0;!!
+  i_r = (int)ceil((x1)/geom1->dr)-1;
+  // TODO: workaround: sometimes it gives -1.
+  // Just get 0 cell if it happence
+  if (i_r < 0) { i_r = 0; }
+  k_z = (int)ceil((x3-0.5*dz)/geom1->dz)-1;
+
+  if(x1>dr)
+    vol_1 = PI*dz*dr*dr*2*i_r;
+  else
+    vol_1 = PI*dz*dr*dr/4.0; //volume of first cell
+
+  r2 = (i_r+0.5)*dr;
+
+  vol_2 = PI*dz*dr*dr*(2*i_r+2);
+  dz1 = (k_z+1.5)*dz - x3;
+  dz2 = x3 - (k_z+0.5)*dz;
+
+  // weighting Hr[i][k]
+  hr = hr + h1_half_time[i_r][k_z]*(PI*dz1*(r2*r2-r1*r1))/vol_1;
+
+  // weighting Hr[i+1][k]
+  hr = hr + h1_half_time[i_r+1][k_z]*PI*dz1*(r3*r3-r2*r2)/vol_2;
+
+  // weighting Hr[i][k+1]
+  hr = hr + h1_half_time[i_r][k_z+1]*PI*dz2*(r2*r2-r1*r1)/vol_1;
+
+  // weighting Hr[i+1][k+1]
+  hr = hr + h1_half_time[i_r+1][k_z+1]*PI*dz2*(r3*r3-r2*r2)/vol_2;
+
+  //// weighting of H_fi
+  // finding number of cell. example dz=0.5, x3 = 0.7, z_k =0;
+  i_r = (int)ceil((x1-0.5*dr)/geom1->dr)-1;
+  // TODO: workaround: sometimes it gives -1.
+  // Just get 0 cell if it happence
+  if (i_r < 0) { i_r = 0; }
+
+  k_z = (int)ceil((x3-0.5*dz)/geom1->dz)-1;
+
+  r2 = (i_r+1)*dr;
+  vol_1 = PI*dz*dr*dr*(2*i_r+1);
+  vol_2 = PI*dz*dr*dr*(2*i_r+3);
+  dz1 = (k_z+1.5)*dz-x3;
+  dz2 = x3-(k_z+0.5)*dz;
+
+  // weighting Hfi[i][k]
+  hfi = hfi + h2_half_time[i_r][k_z]*PI*dz1*(r2*r2-r1*r1)/vol_1;
+
+  // weighting Hfi[i+1][k]
+  hfi = hfi + h2_half_time[i_r+1][k_z]*PI*dz1*(r3*r3-r2*r2)/vol_2;
+
+  // weighting Hfi[i][k+1]
+  hfi = hfi + h2_half_time[i_r][k_z+1]*dz2*PI*(r2*r2-r1*r1)/vol_1;
+
+  // weighting Hfi[i+1][k+1]
+  hfi = hfi + h2_half_time[i_r+1][k_z+1]*PI*dz2*(r3*r3-r2*r2)/vol_2;
+
+  Triple components(hr, hfi, hz);
+
+  return components;
+}
+
+//// /Return one dimensional field components
+double* H_field::get_1d_h1()
+{
+  // copy 2d field array into 1d array rowwise
+#pragma omp parallel for
+  for (int i = 0; i < geom1->n_grid_1; i++)
+    for (int k = 0; k < geom1->n_grid_2 - 1; k++)
+      h1_1d[i * (geom1->n_grid_2 - 1) + k] = h1_half_time[i][k];
+  return h1_1d;
+}
+
+double* H_field::get_1d_h2()
+{
+  // copy 2d field array into 1d array rowwise
+#pragma omp parallel for
+  for (int i = 0; i < geom1->n_grid_1 - 1; i++)
+    for (int k = 0; k < geom1->n_grid_2 - 1; k++)
+      h2_1d[i * (geom1->n_grid_2 - 1) + k] = h2_half_time[i][k];
+  return h2_1d;
+}
+
+double* H_field::get_1d_h3()
+{
+  // copy 2d field array into 1d array rowwise
+#pragma omp parallel for
+  for (int i = 0; i < geom1->n_grid_1 - 1; i++)
+    for (int k = 0; k < geom1->n_grid_2; k++)
+      h3_1d[i * geom1->n_grid_2 + k] = h3_half_time[i][k];
+  return h3_1d;
+}
