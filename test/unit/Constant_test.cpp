7661a011b08741e3513b9acf8986aef21d53d0e1
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-01-31 16:34:39 +0200

    Add simple unit test to check phyysical constants

--- test/unit/Constant_test.cpp
+++ test/unit/Constant_test.cpp
@@ -1,12 +1,15 @@
 #include <gtest/gtest.h>
+#include "Constant.h"
 
-
-// TEST(Addition, CanAddTwoNumbers)
-// {
-//   EXPECT_TRUE(add(2, 2) == 4);
-// }
-
-
+TEST(Universe, isOk)
+{
+  EXPECT_TRUE(constant::PI == 3.1415926535897932);
+  EXPECT_TRUE(constant::EPSILON0 == 8.85E-12);
+  EXPECT_TRUE(constant::EL_MASS == 9.1E-31);
+  EXPECT_TRUE(constant::EL_CHARGE == 1.6E-19);
+  EXPECT_TRUE(constant::LIGHT_SPEED == 3.0E8);
+  EXPECT_TRUE(constant::MAGN_CONST == 1.26E-6);
+}
 
 int main(int ac, char* av[])
 {
