17a73e04f27f480c942dc8001b9df5af14e7930c
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-01-31 14:32:47 +0200

    Remove most of matlab functions as outdated (replaced by python version)

--- matlab/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-CURRENT=${PWD}
-DATA_DIR=$1
-MOVIE_DIR=$2
-
-matlab -nodisplay -r "try; cd('${CURRENT}'); rho_movie_create_light3('${DATA_DIR}', '${MOVIE_DIR}',100,254,2046,[0 1],[0 1],[-1e-7 0]); catch; end; quit"
