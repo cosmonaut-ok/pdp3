cc5d2d4a6c62ead0f86d2047a6da33c9959e3973
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-02-12 12:27:54 +0200

    dev

--- test/unit/PDPTime_test.cpp
+++ test/unit/PDPTime_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "pdp3_time.h"
+#include "pdp3_time.cpp"
 
 // using namespace std;
 
@@ -8,12 +9,16 @@ double start_time = 0;
 double relaxation_time = 0;
 double end_time = 5e-8;
 double delta_t = 1e-12;
-  
-Time* pdp_time = new Time(current_time, start_time, relaxation_time, end_time, delta_t);
 
-TEST(Time, init)
+TEST(Time, time_init)
 {
-  EXPECT_TRUE(2 == 2);
+  Time* pdp_time = new Time(current_time, start_time, relaxation_time, end_time, delta_t);
+
+  EXPECT_TRUE(current_time == pdp_time->current_time);
+  EXPECT_TRUE(start_time == pdp_time->start_time);
+  EXPECT_TRUE(relaxation_time == pdp_time->relaxation_time);
+  EXPECT_TRUE(end_time == pdp_time->end_time);
+  EXPECT_TRUE(delta_t == pdp_time->delta_t);
 }
 
 int main(int ac, char* av[])
