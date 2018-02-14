fa30996fa5fc62b7a9c97985fb4f39e2b207388b
Alexander Vynnyk <cosmonaut-ok@users.noreply.github.com>
2018-02-15 01:31:23 +0200

    Remove pml class 2 (#20)
    * merge PML into Geometry
    
    Add unit test for Geometry
    
    * minor E_field updates, refactoring

--- test/unit/E_field_test.cpp
+++ test/unit/E_field_test.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
 #include "E_field.h"
 #include "E_field.cpp"
+#include "Geometry.h"
 
-TEST(E_field, DUMMY)
-{
-  EXPECT_TRUE(2 == 2);
+namespace e_field {
+
+  double r_size = 0.1;
+  double z_size = 0.4;
+  double n_grid_r = 16;
+  double n_grid_z = 64;
+
+  Geometry * geometry = new Geometry(r_size, z_size, n_grid_r, n_grid_z);
+
+  E_field * efield = new E_field(geometry);
+
+  TEST(E_field, DUMMY)
+  {
+    EXPECT_TRUE(2 == 2);
+  }
 }
