6f6e6f5a9bfbc519cf74840ee7eb36071c3c1408
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-02-16 15:13:50 +0200

    Removed Boundary_Maxwell_conditions::radiation_source and Boundary_Maxwell_conditions::probe_mode_exitation as unused

--- Boundary_Maxwell_conditions.h
+++ Boundary_Maxwell_conditions.h
@@ -12,6 +12,4 @@ public:
   Boundary_Maxwell_conditions(E_field* e_fld);
   ~Boundary_Maxwell_conditions(void);
   void specify_initial_field(Geometry* cyl_geom,double E_fi_upper, double E_fi_left, double E_fi_right);
-  void radiation_source(Geometry* cyl_geom, double region_size, double frequency, int type, double time);
-  void probe_mode_exitation(Geometry* cyl_geom, current* j_input, double probe_lenght, double frequency,double time);
 };
