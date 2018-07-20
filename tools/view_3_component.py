bdb8cf369914b1f4a52d866890f4ced63d182893
Alexander Vynnyk <cosmonaut.ok@gmail.com>
2018-07-20 09:05:05 +0000

    Resolve "tools refactoring"

--- tools/view_3_component.py
+++ tools/view_3_component.py
@@ -11,182 +11,56 @@ from pylab import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 
-from parameters import Parameters
-from pdp3_plot_builder import PDP3PlotBuilder
+from lib.parameters import Parameters
+from lib.pdp3_plot_builder import PDP3PlotBuilder
+
+from lib.pdp_3e_view_builder import Pdp3EViewBuilder
 
 ## README:
 ## color map reference: https://matplotlib.org/examples/color/colormaps_reference.html
 ## mathtext reference:  https://matplotlib.org/users/mathtext.html
 
-class Pdp3View:
+class Pdp3View(Pdp3EViewBuilder):
     def __init__(self, cfg):
-        ## define private object fields
-        self.__cfg = cfg
-        self.__plot_builder = PDP3PlotBuilder(self.__cfg)
+        super(Pdp3View, self).__init__(cfg)
 
-        ## define files data file sets range
-        self.start_data_set = 0
-        self.end_data_set = 1000
-        self.start_frame = 0
-        self.end_frame = 0
-
-        ## define public object fields
-        self.data_file_e_r_pattern = 'e_r'
-        self.data_file_e_z_pattern = 'e_z'
-        self.data_file_e_bunch_density_pattern = 'bunch_density'
-
-        self.x_axis_label = r'$Z (m)$'
-        self.y_axis_label = r'$R (m)$'
-        self.cbar_axis_label = r'$V/m$'
-        self.cbar_bunch_density_axis_label = r'm^{-3}$'
-
-        self.position_e_r = [0.1, 0.70, 0.8, 0.3]
-        self.position_e_z = [0.1, 0.35, 0.8, 0.3]
-        self.position_bunch_density = [0.1, 0.01, 0.8, 0.3]
-
-        ## define public object fields
-        self.cmap = 'gray'
-        self.video_codec = 'mjpeg'
-        self.video_fps = 30
-        self.video_dpi = 100
-        self.video_bitrate=32000
-
-        self.E_r_plot_name = r'$E_r$'
-        self.E_z_plot_name = r'$E_z$'
-        self.E_bunch_density_plot_name = r'$\rho_{beam}$'
-
-    def setup_plot(self):
+    def setup_3e_view(self):
         '''
         initialize plot figure and subplots with preset object fields
         '''
-        self.__plot_builder.setup_figure()
-        ## setup E_r plot
-        self.__plot_builder.add_subplot_with_image(
-            self.E_r_plot_name, 311, cmap=self.cmap, clim=self.__cfg.clim_e_field_r
-        )
-        self.__plot_builder.setup_subplot(
-            self.E_r_plot_name, x_axe_label=self.x_axis_label,
-            y_axe_label=self.y_axis_label, position=self.position_e_r
-        )
-        self.__plot_builder.add_colorbar(
-            self.E_r_plot_name, ticks=self.__cfg.clim_e_field_r, title=self.cbar_axis_label
-        )
-
-        ## setup E_z plot
-        self.__plot_builder.add_subplot_with_image(
-            self.E_z_plot_name, 312, cmap=self.cmap, clim=self.__cfg.clim_e_field_z
-        )
-        self.__plot_builder.setup_subplot(
-            self.E_z_plot_name, x_axe_label=self.x_axis_label,
-            y_axe_label=self.y_axis_label, position=self.position_e_z
-        )
-        self.__plot_builder.add_colorbar(
-            self.E_z_plot_name, ticks=self.__cfg.clim_e_field_z, title=self.cbar_axis_label
-        )
-
-        ## setup bunch_density plot
-        self.__plot_builder.add_subplot_with_image(
-            self.E_bunch_density_plot_name, 313, cmap=self.cmap, clim=self.__cfg.clim_e_field_beam
-        )
-        self.__plot_builder.setup_subplot(
-            self.E_bunch_density_plot_name, x_axe_label=self.x_axis_label,
-            y_axe_label=self.y_axis_label, position=self.position_bunch_density
-        )
-        self.__plot_builder.add_colorbar(
-            self.E_bunch_density_plot_name, ticks=self.__cfg.clim_e_field_beam,
-            ticklabels=[self.__cfg.bunch_density, 0], title=self.cbar_bunch_density_axis_label
-        )
-
-        self.__plot_builder.figure.show()
-
-    def create_view_with_3_plots(self):
-        '''
-        create movie with preset subplots and data from data files
-        '''
-        fpf = self.__cfg.frames_per_file
-        sr = self.__cfg.r_grid_count
-        sz = self.__cfg.z_grid_count
-
-        data_file_e_r = os.path.join(self.__cfg.data_path, self.data_file_e_r_pattern)
-        data_file_e_z = os.path.join(self.__cfg.data_path, self.data_file_e_z_pattern)
-        data_file_bunch_density = os.path.join(self.__cfg.data_path, self.data_file_e_bunch_density_pattern)
-
-        # with writer.saving(self.__plot_builder.figure, movie_file, self.video_dpi):
-        for k in range(self.start_data_set, self.end_data_set+1):
-            tstart = self.start_frame if k == self.start_data_set else k*fpf
-            tend = self.end_frame if k == self.end_data_set else ((k+1)*fpf)
-            i = 1;
-
-            if not os.path.isfile(data_file_e_r + str(k)) \
-               or not os.path.isfile(data_file_e_z + str(k)) \
-               or not os.path.isfile(data_file_bunch_density + str(k)):
-                print('No more data files exists. Exiting')
-                return
-
-            print("Loading files set %d" % (k))
-            ## Open data files
-            fidh_e_r = open(data_file_e_r + str(k), 'r')
-            fidh_e_z = open(data_file_e_z + str(k), 'r')
-            fidh_bunch_density = open(data_file_bunch_density + str(k), 'r')
-
-            h_field_e_r = fromfile(fidh_e_r, dtype=float, count=sr*sz*fpf, sep=' ')
-            h_field_e_z = fromfile(fidh_e_z, dtype=float, count=sr*sz*fpf, sep=' ')
-            h_field_bunch_density = fromfile(fidh_bunch_density, dtype=float, count=sr*sz*fpf, sep=' ')
-
-            ## Close data files
-            fidh_e_r.close()
-            fidh_e_z.close()
-            fidh_bunch_density.close()
-
-            for t in range(tstart, tend+1):
-                local_step = t % fpf
-
-                print("Processing frame %d" % (local_step))
-
-                rstart = sr*sz*local_step
-                rend = sr*sz*(local_step+1)
-
-                try:
-                    self.__plot_builder.fill_image_with_data(
-                        self.E_z_plot_name,
-                        h_field_e_r[rstart:rend])
-
-                    self.__plot_builder.fill_image_with_data(
-                        self.E_r_plot_name,
-                        h_field_e_z[rstart:rend])
-
-                    self.__plot_builder.fill_image_with_data(
-                        self.E_bunch_density_plot_name,
-                        h_field_bunch_density[rstart:rend])
-
-                except ValueError: ## skip frame, when data is inconsistent
-                    break
-
-                self.__plot_builder.redraw()
+        super(Pdp3View, self).setup_3e_view()
+        self._plot_builder.figure.show()
 
 def main():
     ## configure RC properties
     plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
 
     ####
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Tool for visualization of data, generated by PDP3.')
     parser.add_argument('properties_path', metavar='properties_path', type=str,
                         help='Full path to properties.xml')
 
-    parser.add_argument('--video-file', type=str,
-                        help='Full path to output video file')
-
     default_data_set_range = [0, 10000]
-
-    parser.add_argument('--data-set-range', type=str,
-                        help='Range of data files set (e.g. 2:10 is E_r2-Er_10, E_z2-E_z10...). Could be overriden by --time-range and --timestamp. Default %s'
-                        % ':'.join(map(str, default_data_set_range)))
-
-    parser.add_argument('--time-range', type=str, help='Time range. Could be overriden by --timestamp')
+    default_clim = [-1e5, 1e5]
 
     parser.add_argument('--timestamp', type=float, help='Timestamp to generate image at')
 
-    default_clim = [-1e5, 1e5]
+    parser.add_argument('--time-range', type=str, help='Time range. Can be overriden by --timestamp')
+
+    parser.add_argument('--data-set-range', type=str,
+                        help='''Range of data files set (e.g. 2:10 is E_r2 to Er_10, E_z2 to E_z10 and so on).
+                        Can be overriden by --time-range and --timestamp. Default %s'''
+                        % ':'.join(map(str, default_data_set_range)))
+
+    parser.add_argument('--cmap', type=str,
+                        help='''Use custom colormap. Default %s.
+                        Reference: https://matplotlib.org/examples/color/colormaps_reference.html''' % 'gray',
+                        default='gray')
+
+    parser.add_argument('--beam-scale-factor', type=int,
+                        help='''Beam density setting automatically, but you can set scale factor to sets,
+                        where initial bunch density should be placed in color range''',
+                        default=2)
 
     parser.add_argument('--clim-e-r', type=str,
                         help='Color limit range for Electrical field longitual component. Default %s'
@@ -197,65 +71,42 @@ def main():
 
     args = parser.parse_args()
 
-    ## required parameter
-    if not (args.data_set_range or args.time_range or args.timestamp):
-        parser.error('one of --data-set-range or --time-range or --timestamp required')
-
     # check if config file exists
     if os.path.isfile(args.properties_path):
         ## initialize config
-        clim_e_r = list(map(float, args.clim_e_r.split(':'))) if args.clim_e_r else default_clim
-        clim_e_z = list(map(float, args.clim_e_z.split(':'))) if args.clim_e_z else default_clim
-        config = Parameters(args.properties_path, '/dev/null', clim_e_r, clim_e_z)
-
+        config = Parameters(args.properties_path)
         view = Pdp3View(config)
 
         ################################################################################################
         #################### configure plot and view parameters #######################################
         ################################################################################################
+        view.clim_e_field_r = list(map(float, args.clim_e_r.split(':'))) if args.clim_e_r else default_clim
+        view.clim_e_field_z = list(map(float, args.clim_e_z.split(':'))) if args.clim_e_z else default_clim
+        view.cmap = args.cmap
+        view.clim_e_field_beam_scale_factor = args.beam_scale_factor
 
         if args.timestamp:
             view.start_data_set, view.start_frame = config.get_file_frame_by_timestamp(args.timestamp)
             view.end_data_set, view.end_frame = config.get_file_frame_by_timestamp(args.timestamp)
+            view.end_frame = view.end_frame + 1
         elif args.time_range:
             time_range = list(map(float, args.time_range.split(':')))
             view.start_data_set, view.start_frame = config.get_file_frame_by_timestamp(time_range[0])
             view.end_data_set, view.end_frame = config.get_file_frame_by_timestamp(time_range[1])
-        else:
+        elif args.data_set_range:
             data_set_range = list(map(int, args.data_set_range.split(':')))
             view.start_data_set = data_set_range[0]
             view.end_data_set = data_set_range[1]
-
-        view.data_file_e_r_pattern = 'E_r'
-        view.data_file_e_z_pattern = 'E_z'
-        view.data_file_e_bunch_density_pattern = 'rho_beam'
-
-        view.x_axis_label = r'$\mathit{Z (m)}$'
-        view.y_axis_label = r'$\mathit{R (m)}$'
-        view.cbar_axis_label = r'$\frac{V}{m}$'
-        view.cbar_bunch_density_axis_label = r'$m^{-3}$'
-
-        view.position_e_r = [0.1, 0.70, 0.8, 0.3]
-        view.position_e_z = [0.1, 0.35, 0.8, 0.3]
-        view.position_bunch_density = [0.1, 0.01, 0.8, 0.3]
-
-        view.cmap = 'gray'
-        view.video_codec = 'mjpeg'
-        view.video_fps = 30
-        view.video_dpi = 100
-        view.video_bitrate=32000
-
-        view.E_r_plot_name = r'$\mathbf{Electrical\enspace Field\enspace Radial\enspace Component}\enspace(E_r)$'
-        view.E_z_plot_name = r'$\mathbf{Electrical\enspace Field\enspace Longitudal\enspace Component}\enspace(E_z)$'
-        view.E_bunch_density_plot_name = r'$\mathbf{Electron\enspace Bunch\enspace Density}\enspace (\rho_{bunch})$'
+        else:
+            view.start_data_set = default_data_set_range[0]
+            view.end_data_set = default_data_set_range[1]
         ################################################################################################
         ################################################################################################
         ################################################################################################
 
-        view.setup_plot()
+        view.setup_3e_view()
         view.create_view_with_3_plots()
-        if view:
-            input("Press 'Return' to exit ")
+        input("Press 'Return' to exit ")
     else:
         print("Configuration file `%s' does not exists. Exiting" % args.properties_path)
         exit(1)
