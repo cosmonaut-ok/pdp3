09d6f22e0deefe3c5d27b2dc8049a28a32cab5cb
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2018-09-22 17:28:08 +0300

    Add MMX sqrt to speedup calculations

--- include/lib.h
+++ include/lib.h
@@ -4,12 +4,17 @@
 #include <algorithm>
 #include <ctime>
 
+#ifdef __SSE__
+#include <pmmintrin.h>
+#endif
+
 #include "constant.h"
 
 using namespace std;
 
 namespace lib
 {
+  float sqrt_recip(float x);
   bool to_bool(string str);
   double get_gamma (double velocity);
   double get_gamma_inv (double velocity);
