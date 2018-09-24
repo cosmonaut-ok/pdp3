3ca17bf2bd2abcdeac3fd16d95574f78a8691ede
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2018-09-24 19:52:58 +0300

    clear/fix

--- include/particlesList.h
+++ include/particlesList.h
@@ -15,18 +15,7 @@ public:
   vector<Particles*> part_list;
 
 public:
-  /* void charge_weighting(ChargeDensity *ro1); */
-  /* void step_v(EField *e_fld, HField *h_fld, Time *t); */
-  /* void boris_pusher(EField *e_fld, HField *h_fld, Time *t); */
-  /* void half_step_pos(Time *t); */
-  /* void reflection(); */
-  /* void j_weighting(Time *time1, Current *j1); */
-  /* void azimuthal_j_weighting(Time *time1, Current *j1); */
-  /* void dump_position_to_old(void); */
-
-  /* void back_position_to_rz(); */
   void back_velocity_to_rz();
-
 
   void charge_weighting(ChargeDensity *rho);
   void boris_pusher(EField *e_fld, HField *h_fld, Time *t);
