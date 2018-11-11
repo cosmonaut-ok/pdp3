ed1e812d52f29c5604d3ed71b4a9eb972f010c15
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2018-11-11 18:21:51 +0200

    Add directoryExists and makeDirectory functions

--- include/lib.h
+++ include/lib.h
@@ -1,5 +1,8 @@
 #include <sstream>
 #include <iostream>
+#include <string>
+#include <sys/stat.h> // stat
+#include <errno.h> // errno, ENOENT, EEXIST
 #include <math.h>
 #include <algorithm>
 #include <ctime>
@@ -8,7 +11,12 @@
 #include <pmmintrin.h>
 #endif
 
+#ifdef _WIN32
+#include <direct.h> // _mkdir
+#endif
+
 #include "constant.h"
+#include "config.h"
 
 using namespace std;
 
@@ -20,4 +28,6 @@ namespace lib
   double get_gamma_inv (double velocity);
   double random_reverse(double vel, int power);
   char* get_simulation_time();
+  bool directoryExists(const std::string& path);
+  bool makeDirectory(const std::string& path);
 }
