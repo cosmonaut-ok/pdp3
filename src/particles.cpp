78f763704d527537e2c72d0d1e853efad5ef5b93
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2018-12-26 00:20:37 +0200

    update step_v_single
    Former-commit-id: 6b6545aefcb7afb1d493581d7159df047d7a553c

--- src/particles.cpp
+++ src/particles.cpp
@@ -1147,6 +1147,8 @@ void Particles::step_v_single(EField *e_fld, HField *h_fld,
   double vtmp[3];
   double velocity[3];
 
+  double half_dt = t->delta_t / 2.0;
+
   // check if r and z are correct
   if (isnan(pos[i][0]) ||
       isinf(pos[i][0]) != 0 ||
@@ -1158,7 +1160,7 @@ void Particles::step_v_single(EField *e_fld, HField *h_fld,
   }
   //! \f$ const1 = \frac{q t}{2 m} \f$
   //! where \f$ q, m \f$ - particle charge and mass, \f$ t = \frac{\Delta t_{step}}{2} \f$
-  const1 = charge_array[i]*t->delta_t/(2 * mass_array[i]);
+  const1 = charge_array[i] * half_dt / (2 * mass_array[i]);
 
   e = e_fld->get_field(pos[i][0], pos[i][2]);
   b = h_fld->get_field(pos[i][0], pos[i][2]);
